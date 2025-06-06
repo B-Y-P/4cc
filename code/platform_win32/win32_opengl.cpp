1a9ebfe8ea916cbe34ef716276a83d14edab3ae1
BYP <69936251+B-Y-P@users.noreply.github.com>
2025-06-05 21:24:18 -0400

    fix win32/opengl HDC usage for win11 laptops

--- code/platform_win32/win32_opengl.cpp
+++ code/platform_win32/win32_opengl.cpp
@@ -155,7 +155,7 @@ win32_gl_create_window(HWND *wnd_out, HGLRC *context_out, DWORD style, RECT rect
         log_os(" registering graphics class...\n");
         
         WNDCLASSW wndclass = {};
-        wndclass.style = CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS;
+        wndclass.style = CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS|CS_OWNDC;
         wndclass.lpfnWndProc = win32_proc;
         wndclass.hIcon = LoadIconW(GetModuleHandle(0), L"main");
         wndclass.hInstance = this_instance;
